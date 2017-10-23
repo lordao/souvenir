d88723192f47d3f6be2c6366f586c6592b4d1776
Saulo Lordão Andrade Barros <saulo.lordao@gmail.com>
2017-10-23 09:32:03 -0300

    Replaced example API for Souvenir's.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,18 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
 module Lib
     ( startApp
     , app
     ) where
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
-
-$(deriveJSON defaultOptions ''User)
-
-type API = "users" :> Get '[JSON] [User]
+import Souvenir.Link.API
 
 startApp :: IO ()
 startApp = run 8080 app
 
 app :: Application
-app = serve api server
+app = serve api linkV1
 
-api :: Proxy API
+api :: Proxy LinkAPI
 api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
